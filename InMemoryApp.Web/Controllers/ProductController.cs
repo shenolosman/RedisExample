2a7f6c6ef192722959dad1fa4c7e6339bc9e92d8
Shenol <shenol88@hotmail.com>
2024-10-22 15:20:38 +0200

    Add project files.

---
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+        public IActionResult Index()
+        {
+            _memoryCache.Set<string>("time", DateTime.Now.ToString());
+            return View();
+        }
+
+        public IActionResult ShowTime()
+        {
+            ViewBag.Time = _memoryCache.Get<string>("time");
+            return View();
+        }
+    }
+}

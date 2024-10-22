064e772cbc6905776716dc625cd363ec107fb131
Shenol <shenol88@hotmail.com>
2024-10-22 16:02:39 +0200

    to get to know reason with callback delegate

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -19,10 +19,14 @@ namespace InMemoryApp.Web.Controllers
             //if (!_memoryCache.TryGetValue<string>("time", out string timecache))
             //{
             MemoryCacheEntryOptions opt = new MemoryCacheEntryOptions();
-            opt.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
-            opt.SlidingExpiration = TimeSpan.FromSeconds(10);
-            opt.Priority = CacheItemPriority.NeverRemove; //not removes from memory even its full. can be problematic if memory getting full if you set all caches NeverRemove
-            _memoryCache.Set<string>("time", DateTime.Now.ToString());
+            opt.AbsoluteExpiration = DateTime.Now.AddSeconds(5);
+            //opt.SlidingExpiration = TimeSpan.FromSeconds(10);
+            opt.Priority = CacheItemPriority.High; //not removes from memory even its full. can be problematic if memory getting full if you set all caches NeverRemove
+            opt.RegisterPostEvictionCallback((key, value, reason, state) =>
+            {
+                _memoryCache.Set("callback", $"{key}->{value} => reason:{reason}");
+            });
+            _memoryCache.Set<string>("time", DateTime.Now.ToString(), opt);
             //}
 
             return View();
@@ -38,6 +42,8 @@ namespace InMemoryApp.Web.Controllers
             //});
             //or
             _memoryCache.TryGetValue("time", out string timecache);
+            _memoryCache.TryGetValue("callback", out string callbackcache);
+            ViewBag.Callback= callbackcache;
             ViewBag.Time = timecache;
             //ViewBag.Time = _memoryCache.Get<string>("time");
             return View();
